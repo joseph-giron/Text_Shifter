da42ed65ecc24383b3fec59c5de4de3706e89522
averagejoe <joe@gironsec.com>
2019-09-16 20:21:34 -0700

    Add files via upload

---
+++ Text_Shifter/FrmMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace XOR_By
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.tbTextIn = new System.Windows.Forms.TextBox();
+            this.btnShiftBy = new System.Windows.Forms.Button();
+            this.tbShiftBy = new System.Windows.Forms.TextBox();
+            this.tbTextOut = new System.Windows.Forms.TextBox();
+            this.cbShifter = new System.Windows.Forms.ComboBox();
+            this.cvt = new System.ComponentModel.BackgroundWorker();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbTextIn
+            // 
+            this.tbTextIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTextIn.Location = new System.Drawing.Point(12, 12);
+            this.tbTextIn.Multiline = true;
+            this.tbTextIn.Name = "tbTextIn";
+            this.tbTextIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbTextIn.Size = new System.Drawing.Size(330, 176);
+            this.tbTextIn.TabIndex = 0;
+            // 
+            // btnShiftBy
+            // 
+            this.btnShiftBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShiftBy.Location = new System.Drawing.Point(237, 195);
+            this.btnShiftBy.Name = "btnShiftBy";
+            this.btnShiftBy.Size = new System.Drawing.Size(105, 23);
+            this.btnShiftBy.TabIndex = 3;
+            this.btnShiftBy.Text = "Shift By Value";
+            this.btnShiftBy.UseVisualStyleBackColor = true;
+            this.btnShiftBy.Click += new System.EventHandler(this.btnXorBy_Click);
+            // 
+            // tbShiftBy
+            // 
+            this.tbShiftBy.Location = new System.Drawing.Point(163, 196);
+            this.tbShiftBy.Name = "tbShiftBy";
+            this.tbShiftBy.Size = new System.Drawing.Size(68, 20);
+            this.tbShiftBy.TabIndex = 2;
+            // 
+            // tbTextOut
+            // 
+            this.tbTextOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTextOut.Location = new System.Drawing.Point(12, 225);
+            this.tbTextOut.Multiline = true;
+            this.tbTextOut.Name = "tbTextOut";
+            this.tbTextOut.ReadOnly = true;
+            this.tbTextOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbTextOut.Size = new System.Drawing.Size(330, 176);
+            this.tbTextOut.TabIndex = 4;
+            // 
+            // cbShifter
+            // 
+            this.cbShifter.FormattingEnabled = true;
+            this.cbShifter.Items.AddRange(new object[] {
+            "XOR",
+            "OR",
+            "AND",
+            "Bitwise Compliment",
+            "Left Shift",
+            "Right Shift"});
+            this.cbShifter.Location = new System.Drawing.Point(12, 196);
+            this.cbShifter.Name = "cbShifter";
+            this.cbShifter.Size = new System.Drawing.Size(145, 21);
+            this.cbShifter.TabIndex = 1;
+            // 
+            // cvt
+            // 
+            this.cvt.DoWork += new System.ComponentModel.DoWorkEventHandler(this.cvt_DoWork);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(12, 408);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(54, 13);
+            this.lblLength.TabIndex = 5;
+            this.lblLength.Text = "Length: ";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 430);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.cbShifter);
+            this.Controls.Add(this.tbShiftBy);
+            this.Controls.Add(this.btnShiftBy);
+            this.Controls.Add(this.tbTextOut);
+            this.Controls.Add(this.tbTextIn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Text Shifter By AverageJoe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbTextIn;
+        private System.Windows.Forms.Button btnShiftBy;
+        private System.Windows.Forms.TextBox tbShiftBy;
+        private System.Windows.Forms.TextBox tbTextOut;
+        private System.Windows.Forms.ComboBox cbShifter;
+        private System.ComponentModel.BackgroundWorker cvt;
+        private System.Windows.Forms.Label lblLength;
+    }
+}
+
---
+++ Text_Shifter/FrmMain.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace XOR_By
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnXorBy_Click(object sender, EventArgs e)
+        {
+            cvt.RunWorkerAsync();
+        }
+
+        private void cvt_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (tbShiftBy.Text == "")
+            {
+                MessageBox.Show("Missing value to XOR by, please enter something!");
+                return;
+            }
+            if (tbTextIn.Text == "")
+            {
+                MessageBox.Show("Missing text data to XOR by, please enter something!");
+                return;
+            }
+            if (cbShifter.Text == "")
+            {
+                MessageBox.Show("How am I supposed to transform the data? Pick something!");
+                return;
+            }
+            try
+            {
+                int shiftbyval = Convert.ToInt32(tbShiftBy.Text);
+
+                string tmpfile = "bytehax.joe";
+                File.WriteAllText(tmpfile, tbTextIn.Text);
+                switch (cbShifter.Text)
+                {
+                    case "XOR":
+                        byte[] buffer1 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer = new byte[buffer1.Length];
+                        for (int i = 0; i < buffer1.Length; i++)
+                        {
+                            resultBuffer[i] = (byte)(buffer1[i] ^ shiftbyval);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer1.Length.ToString();
+                        break;
+                    case "OR":
+                        byte[] buffer2 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer2 = new byte[buffer2.Length];
+                        for (int i = 0; i < buffer2.Length; i++)
+                        {
+                            resultBuffer2[i] = (byte)(buffer2[i] | shiftbyval);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer2);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer2.Length.ToString();
+                        break;
+                    case "AND":
+                        byte[] buffer3 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer3 = new byte[buffer3.Length];
+                        for (int i = 0; i < buffer3.Length; i++)
+                        {
+                            resultBuffer3[i] = (byte)(buffer3[i] & shiftbyval);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer3);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer3.Length.ToString();
+                        break;
+                    case "Bitwise Compliment":
+                        byte[] buffer4 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer4 = new byte[buffer4.Length];
+                        for (int i = 0; i < buffer4.Length; i++)
+                        {
+                            resultBuffer4[i] = (byte)~Convert.ToInt32(buffer4[i]);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer4);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer4.Length.ToString();
+                        break;
+
+                    case "Left Shift":
+                        byte[] buffer5 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer5 = new byte[buffer5.Length];
+                        for (int i = 0; i < buffer5.Length; i++)
+                        {
+                            resultBuffer5[i] = (byte)(buffer5[i] << shiftbyval);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer5);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer5.Length.ToString();
+                        break;
+                    case "Right Shift":
+                        byte[] buffer6 = File.ReadAllBytes(tmpfile);
+                        byte[] resultBuffer6 = new byte[buffer6.Length];
+                        for (int i = 0; i < buffer6.Length; i++)
+                        {
+                            resultBuffer6[i] = (byte)(buffer6[i] >> shiftbyval);
+                        }
+                        File.WriteAllBytes(tmpfile, resultBuffer6);
+                        tbTextOut.Text = File.ReadAllText(tmpfile);
+                        lblLength.Text = "Length: " + buffer6.Length.ToString();
+                        break;
+                    case "RC4": // not working :(
+                        tbTextOut.Text = RC4(tbTextIn.Text, tbShiftBy.Text);
+                        lblLength.Text = "Length: " + tbTextOut.Text.Length;
+                        break;
+                }
+          
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("just use ints plz");
+                return;
+    }
+            if (File.Exists("bytehax.joe")){ File.Delete("bytehax.joe"); } // cleanup
+  }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public static string RC4(string input, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            int x, y, j = 0;
+            int[] cube = new int[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                cube[i] = i;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                j = (key[i % key.Length] + cube[i] + j) % 256;
+                x = cube[i];
+                cube[i] = cube[j];
+                cube[j] = x;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                y = i % 256;
+                j = (cube[y] + j) % 256;
+                x = cube[y];
+                cube[y] = cube[j];
+                cube[j] = x;
+
+                result.Append((char)(input[i] ^ cube[(cube[y] + cube[j]) % 256]));
+            }
+            return result.ToString();
+        }
+
+
+    }
+}
